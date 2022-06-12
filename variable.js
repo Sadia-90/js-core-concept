90b8e068171b136d88da16935252d87a02b42d77
Sadia-90 <sadiamahmood24@gmail.com>
2022-06-12 16:27:52 +0600

    variable for loop

--- variable.js
+++ variable.js
@@ -1,7 +1,15 @@
-var name = 'sadia';
+// var name = 'sadia';
 
-var condition = false;
+// var condition = false;
+// // console.log(condition);
 
-if(condition){
-   console.log('y! its ff')
+// if (!condition) {
+//    console.log('y! its ff')
+// }
+
+
+// for loop
+
+for(var i =1; i <= 6; i ++){
+    console.log( 'true')
 }
