76d0d83a22247eac2115fa7d27857cb7d76e7f90
Sadia-90 <sadiamahmood24@gmail.com>
2022-06-12 17:46:29 +0600

    array & objects

--- variable.js
+++ variable.js
@@ -10,6 +10,38 @@
 
 // for loop
 
-for(var i =1; i <= 6; i ++){
-    console.log( 'true')
-}
+// for(var i =1; i <= 6; i ++){
+//     console.log( 'true')
+// }
+
+
+// var hour = 21;
+
+// if (hour < 20) {
+//     console.log('good morning')
+// }
+// else {
+//     console.log('good evening')
+// }
+
+// array
+
+// var myMoney = [500, 1000, 1000, 200, 500];
+
+// console.log(myMoney[2]);
+
+// objects
+
+
+// var name = 'rahim';
+// var car = 'toyota';
+// var house = 'rahim vila';
+// var salary = 50000;
+
+// var person = {
+//     name: "rahim",
+//     car: 'toyota',
+//     house: 'rahim villa',
+//     salary: 50000
+// }
+// console.log(person.salary)
