cde4e09aa8786f062c7d57d0fcff9de0cbc09d7d
Sadia-90 <sadiamahmood24@gmail.com>
2022-06-14 16:37:20 +0600

    problem 2

--- problemSolving.js
+++ problemSolving.js
@@ -25,3 +25,25 @@
 // }
 // posNeg(3, 8)
 // posNeg(3, -8)
+
+// no. 3
+//Calculate the sum of salaries within an array of object
+// var persons = [
+//     { name: 'karim', salary: 10000 },
+//     { name: 'rahim', salary: 12000 },
+//     { name: 'deloyar', salary: 2000 },
+//     { name: 'babul', salary: 20000 },
+// ];
+// var sum = 0;
+// for (var i = 0; i < persons.length; i++) {
+//     sum = sum + persons[i].salary
+// }
+// console.log(sum);
+
+
+
+
+let x = 5;
+let y = 2;
+let z = x - y;
+console.log(z);
